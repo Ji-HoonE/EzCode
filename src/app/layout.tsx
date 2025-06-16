29ce1cc0c3b42a76d20f49c432b9c02969ef1a82
pokerbearkr <ogdongwon@gmail.com>
2025-06-16 12:14:22 +0900

    design : favicon 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "EZ-Code",
   description: "임시 description",
+  icons: {
+    icon: "/images/ezcode.ico",
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
