dfd55828dfe8a45996a542b48a85ac1b0730dbc5
grimza99 <gbtmxlf@naver.com>
2025-06-17 15:44:08 +0900

    chore: (headerLayout)->(navigationBarLayout) 으로 이름 수정, 로고 클릭시 홈으로 이동 기능

--- src/widgets/navigation-bar/index.tsx
+++ src/widgets/navigation-bar/index.tsx
@@ -6,7 +6,9 @@ export default function NavigationBar() {
   return (
     <header className="w-full fixed top-0 h-[110px]">
       <div className="flex justify-around items-center">
-        <Image src="/logo/EZcodeLogo.svg" alt="ezCodeLogo.svg" width={200} height={56} />
+        <Link href="/">
+          <Image src="/logo/EZcodeLogo.svg" alt="ezCodeLogo.svg" width={200} height={56} />
+        </Link>
         <Link href={PATHS.CODING_TEST}>문제풀이</Link>
         <Link href={PATHS.COMMUNITY}>커뮤니티</Link>
         <Link href={PATHS.BUSINESS}>기업서비스</Link>
