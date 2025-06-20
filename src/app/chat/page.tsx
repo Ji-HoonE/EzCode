a27f19953ae8bab938fe765d94355a6b446b5f00
grimza99 <gbtmxlf@naver.com>
2025-06-21 00:13:04 +0900

    feat: ChatPage에서 useConnectWebSocket로 연결

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+import { useConnectWebSocket } from '@/features/chat/hooks/useConnectWebSocket';
+import { StompInitialRoomsType } from '@/features/chat/types/stomp';
+import { useEffect, useState } from 'react';
+
 export default function ChatPage() {
-  return <div>사실 이건 모달처럼 보이지만 채팅페이지임</div>;
+  const [rooms, setRooms] = useState<StompInitialRoomsType>([]);
+
+  const stompRef = useConnectWebSocket(
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1IiwiZW1haWwiOiJnYnRteGxmQG5hdmVyLmNvbSIsInVzZXJuYW1lIjoi7Jyg7ISg7ZalIiwibmlja25hbWUiOiLsnKDshKDtlqUiLCJ1c2VyUm9sZSI6IkFETUlOIiwidGllciI6Ik5FV0JJRSIsImV4cCI6MTc1MTAyNzQ5NywiaWF0IjoxNzUwNDIyNjk3fQ.GYBJkFE4Q_GyiRNjefuyanChGs4h0LJuRyzIrHTdJE0'
+  );
+
+  useEffect(() => {
+    if (!localStorage) return;
+    const chatRooms = localStorage.getItem('chatRooms')
+      ? (JSON.parse(localStorage.getItem('chatRooms')!) as StompInitialRoomsType)
+      : [];
+
+    setRooms(chatRooms);
+  }, [stompRef]);
+
+  return (
+    <div>
+      {rooms.length > 0 ? (
+        rooms.map((room) => {
+          return (
+            <div key={room.roomId}>
+              <p>{room.title}</p> <p>{room.roomId}</p>
+            </div>
+          );
+        })
+      ) : (
+        <div>생성된 채팅방이 없습니다.</div>
+      )}
+    </div>
+  );
 }
