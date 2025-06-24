bdad453cb7485bab1aeb355872b42cec5cf154dd
grimza99 <gbtmxlf@naver.com>
2025-06-24 15:14:32 +0900

    feat: 로그인 기본 폼과 타입 생성

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,2 +1,5 @@
-export { default as LoginForm } from './ui/LoginForm';
+export { default as SigninForm } from './ui/SigninForm';
 export { default as SignupForm } from './ui/SignupForm';
+
+//model
+export { DEFAULT_SIGNIN_FORM_VALUE } from './model/defaultFormValues';
---
+++ src/features/auth/model/defaultFormValues.ts
@@ -0,0 +1,6 @@
+import { SigninForm } from '../types';
+
+export const DEFAULT_SIGNIN_FORM_VALUE: SigninForm = {
+  email: 'string',
+  password: 'string',
+};
