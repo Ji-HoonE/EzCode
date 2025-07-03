8bf8bfe4c6d16b04e488db61d952c77ba433451a
grimza99 <gbtmxlf@naver.com>
2025-07-03 20:01:47 +0900

    feat: 기존 public 아이콘 svgr 컴포넌트로 변경, 적용

--- src/features/problem/ui/TerminalPanel.tsx
+++ src/features/problem/ui/TerminalPanel.tsx
@@ -1,9 +1,9 @@
 'use client';
-import Image from 'next/image';
 import { submitSourceCodeData } from '../lib/submitSourceCodeData';
 import { useState } from 'react';
 import useConnectProblemWebSocket from '../hooks/useConnectProblemWebSocket';
 import { IProblemRequestData } from '../types/problem.request.data.type';
+import { TerminalResultIcon, TerminalReviewIcon, TerminalRunIcon } from '@/shared/ui/icons';
 
 interface TerminalPanelProps {
   problemId: string;
@@ -26,30 +26,15 @@ export default function TerminalPanel({ problemId, sourceCodeData }: TerminalPan
     <div className="flex flex-col w-[68px] px-[10px] pt-[19px]">
       <div className="flex flex-col gap-6 items-center w-full">
         <div className="flex flex-col gap-[3px] items-center" onClick={handleSubmitSourceCodeData}>
-          <Image
-            src="/icons/terminal-icons/terminal-run-icon.svg"
-            alt="run"
-            height={34}
-            width={30}
-          />
+          <TerminalRunIcon className="text-[#ffffff]" />
           <h3 className="text-[10px]">RUN</h3>
         </div>
         <div className="flex flex-col gap-2 items-center w-[30px]">
-          <Image
-            src="/icons/terminal-icons/terminal-result-icon.svg"
-            alt="run"
-            height={14}
-            width={23}
-          />
+          <TerminalResultIcon className="text-[#00E35B]" />
           <h3 className="text-[10px]">RESULT</h3>
         </div>
         <div className="flex flex-col gap-[3px] items-center">
-          <Image
-            src="/icons/terminal-icons/terminal-review-icon.svg"
-            alt="run"
-            height={34}
-            width={30}
-          />
+          <TerminalReviewIcon className="text-[#6B6B6B]" />
           <h3 className="text-[10px]">REVIEW</h3>
         </div>
       </div>
