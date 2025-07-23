46c0c66216f209de6b21b7bd5c6bf648b765cce6
grimza99 <gbtmxlf@naver.com>
2025-07-24 05:38:31 +0900

    refactoring : page.tsx 에 주석처리된 로그아웃 뮤테이션 이동

--- src/app/(auth)/(navigationsBarLayout)/page.tsx
+++ src/app/(auth)/(navigationsBarLayout)/page.tsx
@@ -1,44 +1,10 @@
-// import ApiHelper from '@/api/client/api';
-// import { API_CONSTANTS } from '@/api/constants/api.constants';
 import { getWeeklyRankings } from '@/entities/rankings/actions/getRankings.actions';
 import Rankings from '@/entities/rankings/ui/Rankings';
 import ChatDialogOpenButton from '@/features/chat/ui/ChatDialogOpenButton';
-// import { useLogoutMutation } from '@/query/auth/auth';
-
 import { StartButton } from '@/widgets/landingCTA';
-// import { useQueryClient } from '@tanstack/react-query';
-// import { signOut } from 'next-auth/react';
 import Image from 'next/image';
-// import { useEffect } from 'react';
 
 export default async function HomePage() {
-  // const { mutateAsync } = useLogoutMutation();
-  // const queryClient = useQueryClient();
-
-  // // TODO 로그아웃 api 임의연동
-  // const handleLogout = async () => {
-  //   try {
-  //     const res = await mutateAsync();
-  //     if (res.data.status === API_CONSTANTS.CODE.OK) {
-  //       await signOut({ redirect: true, callbackUrl: '/signin' });
-  //       // 로그아웃 시 내정보조회하는 api 캐시 무효화
-  //       queryClient.invalidateQueries({ queryKey: ['my-info'] });
-  //       queryClient.invalidateQueries({ queryKey: ['my-ranking'] });
-  //       queryClient.invalidateQueries({ queryKey: ['my-review'] });
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-  // // TODO 테스트 유저정보 api 임의연동
-  // useEffect(() => {
-  //   const fetchUserInfo = async () => {
-  //     const response = await ApiHelper.get('/users', { reqType: 'client' });
-  //     console.log('response', response);
-  //   };
-  //   fetchUserInfo();
-  // }, []);
-
   const weeklyRankings = await getWeeklyRankings();
 
   return (
@@ -59,11 +25,8 @@ export default async function HomePage() {
           </div>
         </div>
       </section>
-      <section>
-        <h2>이번 주 랭킹 TOP 10</h2>
-        <p>이번 주 가장 활발하게 문제를 해결한 개발자들</p>
-        <Rankings rankings={weeklyRankings} />
-      </section>
+
+      <Rankings rankings={weeklyRankings} />
     </main>
   );
 }
