1055ce2fa1221a8e015a104c3332bc4bcd45f9b0
grimza99 <gbtmxlf@naver.com>
2025-08-12 22:17:38 +0900

    fix : 빌드 에러 해결

--- src/shared/lib/zod/useZodForm.ts
+++ src/shared/lib/zod/useZodForm.ts
@@ -11,7 +11,7 @@ import z from 'zod';
 export const useZodForm = (schemaName: z.ZodObject, names: TZodKey[]) => {
   const dynamicDefaultValue: Record<string, string | number> = {};
 
-  for (let el of names) {
+  for (const el of names) {
     if (names.length < 1) throw new Error('useZodForm의 names를 넘겨주세요');
     dynamicDefaultValue[el] = ZOD_DEFAULT_VALUES[el];
   }
