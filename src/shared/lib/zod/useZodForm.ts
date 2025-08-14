ff46fb3a1ad0377bcf730d44d1fca3203ad5abc9
grimza99 <gbtmxlf@naver.com>
2025-08-14 18:39:25 +0900

    refactoring : useZodForm undefined 오류 방어

--- src/shared/lib/zod/useZodForm.ts
+++ src/shared/lib/zod/useZodForm.ts
@@ -3,7 +3,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { TZodKey } from './types';
 import { ZOD_DEFAULT_VALUES } from './defaultValues';
-import z from 'zod';
+import { z } from 'zod';
 
 /**@description schemaName- 작성한 z.object 스카마의 이름 */
 /**@description name- 작성한 TZodKey의 배열 */
@@ -11,8 +11,10 @@ import z from 'zod';
 export const useZodForm = (schemaName: z.ZodObject, names: TZodKey[]) => {
   const dynamicDefaultValue: Record<string, string | number> = {};
 
+  if (names.length < 1) throw new Error('useZodForm의 names를 넘겨주세요');
+
   for (const el of names) {
-    if (names.length < 1) throw new Error('useZodForm의 names를 넘겨주세요');
+    if (!!ZOD_DEFAULT_VALUES[el]) throw new Error('ZOD_DEFAULT_VALUES를 작성해 주세요');
     dynamicDefaultValue[el] = ZOD_DEFAULT_VALUES[el];
   }
 
