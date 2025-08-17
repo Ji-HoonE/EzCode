9b1a5969cac5c03219c6ef99197c72c6944ef31e
JiHoon <gkwlgnd@naver.com>
2025-08-17 21:53:05 +0900

    fix : 게임채팅아이콘 위치수정

--- src/widgets/globalFloatingWidget/ui/index.tsx
+++ src/widgets/globalFloatingWidget/ui/index.tsx
@@ -11,7 +11,7 @@ export function GlobalFloatingWidget() {
   const { data: characterData } = useCheckCharacterQuery(!!isLogged);
 
   return (
-    <div className="fixed top-[calc(100vh-15%)] left-[calc(100vw-10%)] flex flex-col gap-2">
+    <div className="fixed top-[calc(100vh-15%)] left-[calc(100vw-5%)] flex flex-col gap-2">
       {isLogged && <ChatDialogOpenButton />}
       {isLogged && (
         <GameModalButton hasCharacter={characterData?.data?.result?.isCharacterExist || false} />
