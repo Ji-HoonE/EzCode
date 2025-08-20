4ecfa6cc09e1171201234d3864d8971c88ec85d7
grimza99 <gbtmxlf@naver.com>
2025-08-20 22:55:22 +0900

    feat : protectedBlurBox 에서 로그인 모달 띄우고, 로그인 이후 useSession 업데이트로 블러박스 해제

--- src/shared/ui/LoginRequiredUi/ProtectedBlurBox.tsx
+++ src/shared/ui/LoginRequiredUi/ProtectedBlurBox.tsx
@@ -1,20 +1,31 @@
+'use client';
 import { Button } from '@/components/ui/button';
-import { PATHS } from '@/constants/paths';
-import clsx from 'clsx';
-import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function ProtectedBlurBox() {
+interface IProtectedBlurBoxProps {
+  className?: string;
+}
+export default function ProtectedBlurBox({ className }: IProtectedBlurBoxProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const authGuardTrigger = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('auth-guard', 'true');
+    router.push(`?${params.toString()}`);
+  };
+
   return (
     <div
-      className={clsx(
-        'flex justify-center items-center bg-white/20 backdrop-blur-sm pointer-events-auto rounded-md absolute z-25 inset-0'
+      className={cn(
+        'flex justify-center items-center bg-white/20 backdrop-blur-sm pointer-events-auto rounded-md absolute z-25 inset-0',
+        className
       )}
     >
       <div className="flex flex-col gap-3 items-center">
         <p>로그인이 필요한 서비스에요!</p>
-        <Link href={PATHS.SIGNIN}>
-          <Button>로그인 하러 가기</Button>
-        </Link>
+        <Button onClick={authGuardTrigger}>로그인 하기</Button>
       </div>
     </div>
   );
