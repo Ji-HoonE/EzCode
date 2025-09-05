fcc4a68534d66cdaef8641c98a7aa7e5aeb1e02f
grimza99 <gbtmxlf@naver.com>
2025-09-06 01:30:43 +0900

    feat: Markdown 렌더러 추가 및 FormatMarkdown 컴포넌트 제거

---
+++ src/shared/mdx/MdxRenderer.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+type Props = { source: string };
+
+export default function MarkdownRenderer({ source }: Props) {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        h2: ({ node, ...props }) => (
+          <h2 className="text-xl font-semibold mt-3 text-secondary" {...props} />
+        ),
+        h3: ({ node, ...props }) => (
+          <h3 className="text-md font-semibold text-secondary mt-3" {...props} />
+        ),
+        strong: ({ node, ...props }) => <strong className="font-bold text-md" {...props} />,
+      }}
+    >
+      {source}
+    </ReactMarkdown>
+  );
+}
