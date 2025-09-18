68d4d0dca639f827a0d41d2960de1aa56b7f0d42
grimza99 <gbtmxlf@naver.com>
2025-09-18 16:41:43 +0900

    fix : 이미지 설정을 위한 next.config 설정 오탈자 수정

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false, // Strict Mode 비활성화
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'ezcode-s3.s3.ap-northeast-2.amazonaws.com',
-      pathname: '/**',
-    },
-  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ezcode-s3.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
