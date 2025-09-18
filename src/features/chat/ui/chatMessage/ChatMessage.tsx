73d3c58ce67eb0932d250ae8399b6c16fbb28e74
grimza99 <gbtmxlf@naver.com>
2025-09-18 16:49:57 +0900

    fix : 채팅을 보낸 유저 이름 추가

--- src/features/chat/ui/chatMessage/ChatMessage.tsx
+++ src/features/chat/ui/chatMessage/ChatMessage.tsx
@@ -9,14 +9,14 @@ export default function ChatMessage({ msg, userNickname }: IChatMessageProps) {
   return (
     <li className={`flex ${isOwn ? 'justify-end' : 'justify-start'}`}>
       <div className={`max-w-[70%] ${isOwn ? 'order-2' : 'order-1'}`}>
+        <p className={`text-sm ${isOwn ? 'text-right' : 'text-left'}`}>{userNickname}</p>
+
         <div
-          className={`px-4 py-2 rounded-[14px] ${
-            isOwn ? 'bg-[#214d35] text-white' : 'bg-[#1a2332] text-white'
-          }`}
+          className={`px-4 py-2 rounded-[14px] ${isOwn ? 'bg-primary ' : 'bg-secondary-background '}`}
         >
           <p className="whitespace-pre-wrap break-words">{msg.message}</p>
         </div>
-        <p className={`text-xs text-[#888] mt-1 ${isOwn ? 'text-right' : 'text-left'}`}>
+        <p className={`text-xs text-gray500 mt-1 ${isOwn ? 'text-right' : 'text-left'}`}>
           {formattedDate}
         </p>
       </div>
