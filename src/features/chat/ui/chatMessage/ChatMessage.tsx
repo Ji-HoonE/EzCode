92c8f979462ff75f7e0d5c80315952c67d667e61
grimza99 <gbtmxlf@naver.com>
2025-09-18 17:19:45 +0900

    fix : 채팅 메시지 레이아웃 개선 및 정렬 수정

--- src/features/chat/ui/chatMessage/ChatMessage.tsx
+++ src/features/chat/ui/chatMessage/ChatMessage.tsx
@@ -7,19 +7,16 @@ export default function ChatMessage({ msg, userNickname }: IChatMessageProps) {
   const formattedDate = formatDate(msg.time);
 
   return (
-    <li className={`flex ${isOwn ? 'justify-end' : 'justify-start'}`}>
-      <div className={`max-w-[70%] ${isOwn ? 'order-2' : 'order-1'}`}>
-        <p className={`text-sm ${isOwn ? 'text-right' : 'text-left'}`}>{msg.name}</p>
-
-        <div
-          className={`px-4 py-2 rounded-[14px] ${isOwn ? 'bg-primary ' : 'bg-secondary-background '}`}
-        >
-          <p className="whitespace-pre-wrap break-words">{msg.message}</p>
-        </div>
-        <p className={`text-xs text-gray500 mt-1 ${isOwn ? 'text-right' : 'text-left'}`}>
-          {formattedDate}
-        </p>
+    <li className={`flex flex-col ${isOwn ? 'items-end' : 'items-start'}`}>
+      <p className={`text-sm ${isOwn ? 'text-right' : 'text-left'}`}>{msg.name}</p>
+      <div
+        className={`max-w-[70%] w-fit px-4 py-2 rounded-[14px] ${isOwn ? ' bg-primary' : ' bg-secondary-background'}`}
+      >
+        <p className="whitespace-pre-wrap break-words">{msg.message}</p>
       </div>
+      <p className={`text-xs text-gray500 mt-1 ${isOwn ? 'text-right' : 'text-left'}`}>
+        {formattedDate}
+      </p>
     </li>
   );
 }
